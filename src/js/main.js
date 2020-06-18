918448a8e6e4570ce98b46ebc86f5447ba9a9fba
Mriox <eduard.z4ck@mail.ru>
2020-06-18 23:48:00 +0300

    Show main screens for game start

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,53 @@
-// для теста
+// import './player.js';
 
-/* import './player.js';
+let app = document.querySelector(`body .app`);
+let main = document.querySelector(`body .app .main`);
+let template = document.querySelector(`template`).content;
+let screens = template.querySelectorAll(`.main`);
+let arrows = template.querySelector(`.arrows__wrap`);
 
-function name(params) {
-  console.log(`object`);
+export const key = {
+  leftArrow: 37,
+  rightArrow: 39
+};
+
+
+function switchBlock(sections, appendTo) { // загружает нужный блок из темплейтов
+  let number = 0;
+
+  return function (plusMinus = 0) {
+    number += plusMinus;
+    if (number < 0 || number > 5) {
+      number -= plusMinus;
+      return;
+    }
+
+    sections = [...sections];
+    appendTo.innerHTML = ``;
+    appendTo.appendChild(sections[number]);
+  };
 }
-name();
-console.log(123); */
+
+function onArrowsPress(evt) { // переключает блоки по клавишам стрелок
+  evt.keyCode === key.leftArrow ? closuredSwitch(-1) : false;
+  evt.keyCode === key.rightArrow ? closuredSwitch(1) : false;
+}
+
+let closuredSwitch = switchBlock(screens, main);
+
+let onContentLoaded = () => {
+  app.appendChild(arrows);
+  closuredSwitch();
+
+  let arrowsDiv = app.querySelector(`.arrows__wrap`);
+  arrowsDiv.addEventListener(`click`, function (evt) {
+    evt.target.classList.contains(`left`) ? closuredSwitch(-1) : false;
+    evt.target.classList.contains(`right`) ? closuredSwitch(1) : false;
+  });
+};
+
+
+document.addEventListener(`DOMContentLoaded`, onContentLoaded);
+document.addEventListener(`keydown`, onArrowsPress);
+
+
